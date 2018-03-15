b2b3a50feea375d656ae74797550fe5bc3329258
vovanz <vovanzagorodsky@gmail.com>
2018-03-15 18:34:38 +0500

    implement main feature and add readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# What is this?
+
+This is a tool that makes you leave all public chats and channels in Telegram.
+Can be useful if you waste too much time reading this shit.
+
+# Usage
+
+1. Get `API_ID` and `API_HASH` here: https://my.telegram.org
+2. Create `local_vars` file that looks like this:
+
+```bash
+export API_ID=12345
+export API_HASH="12345abcde"
+```
+3. `pip install -r requirements.txt`
+4. `source local_vars`
+5. `./main.py`
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 from telethon import TelegramClient
+from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon.tl.types import Channel
 
 from config import API_ID, API_HASH
 
 client = TelegramClient('experimental_client', API_ID, API_HASH)
 client.start()
+
+dialogs = client.get_dialogs()
+
+for dialog in dialogs:
+    if type(dialog.entity) is Channel:
+        print(dialog.name)
+        client(LeaveChannelRequest(dialog.entity))
